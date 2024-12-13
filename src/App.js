7d3e30b0f24d6586c56608f5dc9197476fdb28f4
unknown <psialex79>
2024-12-13 12:51:07 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Time Management App</h1>
+      <p>Добро пожаловать! Здесь вы можете управлять своим временем.</p>
+    </div>
+  );
+}
+
+export default App;
