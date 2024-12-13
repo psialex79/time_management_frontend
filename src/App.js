c1642d22369062367968e4ab6699c4b92568fbcd
unknown <psialex79>
2024-12-13 13:17:52 +0700

    fix/styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import "./styles/App.css";
+
 function App() {
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Time Management App</h1>
-      <p>Добро пожаловать! Здесь вы можете управлять своим временем.</p>
+    <div className="app-container">
+      <h1 className="app-title">Time Management App</h1>
+      <p className="app-description">
+        Добро пожаловать! Здесь вы можете управлять своим временем.
+      </p>
     </div>
   );
 }
