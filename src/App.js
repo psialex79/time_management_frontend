4e8fceecbae187a9d433dfa7c2250d7b71d90627
unknown <psialex79>
2024-12-13 13:29:19 +0700

    fix/styles

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ function App() {
   return (
     <div className="app-container">
       <h1 className="app-title">Time Management App</h1>
-      <p className="app-description">
-        Добро пожаловать! Здесь вы можете управлять своим временем.
-      </p>
       <AnimatedBox />
     </div>
   );
