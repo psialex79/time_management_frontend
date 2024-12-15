2e86353524f707c6abc796614ef624bd62ea613c
unknown <psialex79>
2024-12-15 22:12:57 +0700

    fix/modal

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -14,37 +14,14 @@ function WelcomePage({ backendResponse }) {
     }
   }, [backendResponse]);
 
-  // Получение текущей даты в формате YYYY-MM-DD
-  const getTodayDate = () => {
-    const today = new Date();
-    return today.toISOString().split("T")[0];
-  };
-
-  // Проверяем, является ли postAuthResponse массивом
-  const isResponseArray = Array.isArray(postAuthResponse);
-
-  // Фильтрация записей с сегодняшней датой, если response - массив
-  const todayRecords = isResponseArray
-    ? postAuthResponse.filter((record) => record.record_date === getTodayDate())
-    : [];
-
   return (
     <div className="welcome-container">
       {isLoading ? <StatusMessage message="Загрузка..." /> : null}
 
       {/* Если записи присутствуют, отображаем их */}
-      {todayRecords.length > 0 ? (
-        <RecordsList records={todayRecords} />
-      ) : (
-        !isLoading &&
-        (isResponseArray ? (
-          <StatusMessage message="Сегодня записей нет" />
-        ) : (
-          <StatusMessage
-            message={postAuthResponse.message || "Ошибка данных"}
-          />
-        ))
-      )}
+      {postAuthResponse && postAuthResponse.length > 0 ? (
+        <RecordsList records={postAuthResponse} />
+      ) : null}
     </div>
   );
 }
