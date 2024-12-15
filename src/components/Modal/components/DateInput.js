4f04775e6a6cd408457aed3a6ea1a5248ff24063
unknown <psialex79>
2024-12-15 16:00:51 +0700

    fix/modal styles

--- src/components/Modal/components/DateInput.js
+++ src/components/Modal/components/DateInput.js
@@ -1,23 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function DateInput({ value, onChange }) {
-  const [currentDate, setCurrentDate] = useState("");
-
-  useEffect(() => {
-    const today = new Date();
-    const formattedDate = today.toISOString().split("T")[0]; // Формат: YYYY-MM-DD
-    setCurrentDate(formattedDate);
-  }, []);
-
   return (
     <div className="input-field">
-      <input
-        type="date"
-        id="date"
-        value={value || currentDate}
-        onChange={onChange}
-        required
-      />
+      <input type="date" id="date" value={value} onChange={onChange} required />
     </div>
   );
 }
