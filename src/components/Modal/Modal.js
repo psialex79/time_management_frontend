f252fbdfa09aaead385bc3e64cd60de6eb32602a
unknown <psialex79>
2024-12-15 19:17:32 +0700

    fix/modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -11,9 +11,20 @@ function Modal({ isOpen, onClose }) {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const [name, setName] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // Проверка на обязательные поля
+    if (!date || !time || !name) {
+      setError("Пожалуйста, заполните все поля.");
+      return;
+    }
+
+    setError(""); // Сбрасываем ошибку, если все поля заполнены
+    setIsSubmitting(true); // Делаем кнопку неактивной, когда данные отправляются
 
     const formData = { date, time, name };
 
@@ -22,19 +33,20 @@ function Modal({ isOpen, onClose }) {
       if (initData) {
         formData.initData = initData;
       }
+
+      const response = await sendFormData(formData);
+
+      if (response) {
+        console.log("Данные успешно отправлены:", response);
+      } else {
+        console.error("Ошибка при отправке данных.");
+      }
     } catch (error) {
-      console.error("Ошибка при получении initData:", error);
+      console.error("Ошибка при отправке данных:", error);
+    } finally {
+      setIsSubmitting(false); // Разблокируем кнопку после отправки данных
+      onClose();
     }
-
-    const response = await sendFormData(formData);
-
-    if (response) {
-      console.log("Данные успешно отправлены:", response);
-    } else {
-      console.error("Ошибка при отправке данных.");
-    }
-
-    onClose();
   };
 
   if (!isOpen) return null;
@@ -48,11 +60,17 @@ function Modal({ isOpen, onClose }) {
           <NameInput value={name} onChange={(e) => setName(e.target.value)} />
         </form>
 
+        {/* Отображение ошибки, если форма не заполнена */}
+        {error && <div className="error-message">{error}</div>}
+
         <div className="modal-buttons-container">
           <button className="modal-close-btn" onClick={onClose}>
             х
           </button>
-          <SubmitButton onClick={handleSubmit} />
+          <SubmitButton
+            onClick={handleSubmit}
+            disabled={isSubmitting} // Блокируем кнопку при отправке
+          />
         </div>
       </div>
     </div>
