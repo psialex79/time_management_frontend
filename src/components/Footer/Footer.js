9fc6a089e6ed539c0cce7658c84d0dc411c9c773
unknown <psialex79>
2024-12-15 16:58:15 +0700

    feature/header, footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer-text">©2024</p>
-    </footer>
-  );
+  return <footer className="footer"></footer>;
 }
 
 export default Footer;
