324c162bf2a1f2c00ab7a6c7b00355a8ee8add6e
unknown <psialex79>
2024-12-17 22:39:33 +0700

    fix/modal form

--- src/components/Modal/components/DateInput.js
+++ src/components/Modal/components/DateInput.js
@@ -1,10 +1,53 @@
-import React, { forwardRef } from "react";
+import React, { useState, forwardRef } from "react";
 
-const DateInput = forwardRef(({ error, ...props }, ref) => (
-  <div className="input-field">
-    <input type="date" id="date" ref={ref} {...props} required />
-    {error && <span className="error-text">{error}</span>}
-  </div>
-));
+const DateInput = forwardRef(({ error, ...props }, ref) => {
+  const [isSelectorVisible, setIsSelectorVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState("");
+
+  const handleDateChange = (e) => {
+    setSelectedDate(e.target.value);
+  };
+
+  const handleBlur = () => {
+    if (selectedDate) {
+      setIsSelectorVisible(false);
+    }
+  };
+
+  return (
+    <div className="input-field">
+      {!isSelectorVisible ? (
+        selectedDate ? (
+          <div
+            className="selected-date"
+            onClick={() => setIsSelectorVisible(true)}
+          >
+            {selectedDate} <span className="edit-text">(Изменить)</span>
+          </div>
+        ) : (
+          <button
+            type="button"
+            onClick={() => setIsSelectorVisible(true)}
+            className="select-date-button"
+          >
+            Выберите дату
+          </button>
+        )
+      ) : (
+        <input
+          type="date"
+          ref={ref}
+          {...props}
+          value={selectedDate}
+          onChange={handleDateChange}
+          onBlur={handleBlur}
+          autoFocus
+          required
+        />
+      )}
+      {error && <span className="error-text">{error}</span>}
+    </div>
+  );
+});
 
 export default DateInput;
