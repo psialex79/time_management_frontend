16718886f5b14f9754e6a163f4a9accd48f56b3c
unknown <psialex79>
2024-12-18 16:52:38 +0700

    fix/styles

--- src/components/AddRecordPage/AddRecordPage.js
+++ src/components/AddRecordPage/AddRecordPage.js
@@ -31,34 +31,36 @@ function AddRecordPage() {
   };
 
   return (
-    <div className="add-record-page">
-      <form onSubmit={handleSubmit(onSubmit)} className="add-record-form">
-        <DateInput
-          {...register("date", { required: "Укажите дату." })}
-          error={errors.date?.message}
-          value={watch("date")}
-          onChange={(e) => setValue("date", e.target.value)}
-        />
-        <TimeInput
-          {...register("time", { required: "Укажите время." })}
-          error={errors.time?.message}
-          value={watch("time")}
-          onChange={(e) => setValue("time", e.target.value)}
-        />
-        <NameInput
-          {...register("name", { required: "Укажите описание." })}
-          error={errors.name?.message}
-        />
-        <div className="buttons-container">
-          <SubmitButton type="submit" disabled={isSubmitting} />
-        </div>
-      </form>
+    <div className="main-container">
+      <div className="add-record-page">
+        <form onSubmit={handleSubmit(onSubmit)} className="add-record-form">
+          <DateInput
+            {...register("date", { required: "Укажите дату." })}
+            error={errors.date?.message}
+            value={watch("date")}
+            onChange={(e) => setValue("date", e.target.value)}
+          />
+          <TimeInput
+            {...register("time", { required: "Укажите время." })}
+            error={errors.time?.message}
+            value={watch("time")}
+            onChange={(e) => setValue("time", e.target.value)}
+          />
+          <NameInput
+            {...register("name", { required: "Укажите описание." })}
+            error={errors.name?.message}
+          />
+          <div className="buttons-container">
+            <SubmitButton type="submit" disabled={isSubmitting} />
+          </div>
+        </form>
 
-      {Object.keys(errors).length > 0 && (
-        <div className="error-message">
-          Пожалуйста, исправьте ошибки в форме.
-        </div>
-      )}
+        {Object.keys(errors).length > 0 && (
+          <div className="error-message">
+            Пожалуйста, исправьте ошибки в форме.
+          </div>
+        )}
+      </div>
     </div>
   );
 }
