33e140b22d12d9943c5140157dbada2c7aa82984
unknown <psialex79>
2024-12-18 20:13:33 +0700

    fix/footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import PlusIconButton from "./components/PlusIconButton";
 import BackIconButton from "./components/BackIconButton";
 import "./Footer.css";
 
 function Footer({ submitHandler }) {
   const navigate = useNavigate();
+  const location = useLocation(); // Определяем текущий путь
 
   const goToWelcomePage = () => {
     navigate("/");
   };
 
   const handleAddRecord = async () => {
-    if (submitHandler) {
-      await submitHandler();
+    if (location.pathname === "/add-record") {
+      // Если мы на странице добавления записи
+      if (submitHandler) {
+        await submitHandler(); // Выполняем отправку данных
+      }
+      goToWelcomePage(); // Возвращаемся на главную страницу
+    } else {
+      // Если мы на главной странице
+      navigate("/add-record"); // Переходим на страницу добавления записи
     }
-    goToWelcomePage();
   };
 
   return (
