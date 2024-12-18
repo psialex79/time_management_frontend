b6eeea1e20243ff7b527d2b5d6c078429c5f51a4
unknown <psialex79>
2024-12-18 21:56:43 +0700

    fix/disable enter

--- src/components/AddRecordPage/components/NameInput.js
+++ src/components/AddRecordPage/components/NameInput.js
@@ -1,17 +1,24 @@
 import React, { forwardRef } from "react";
 
-const NameInput = forwardRef(({ error, ...props }, ref) => (
-  <div className="input-field">
-    <input
-      type="text"
-      id="name"
-      ref={ref}
-      {...props}
-      placeholder="Контакт и комментарий."
-      required
-    />
-    {error && <span className="error-text">{error}</span>}
-  </div>
-));
+const NameInput = forwardRef(({ error, ...props }, ref) => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
 
-export default NameInput;
+  return (
+    <div className="input-field">
+      <input
+        type="text"
+        id="name"
+        ref={ref}
+        {...props}
+        placeholder="Контакт и комментарий."
+        required
+        onKeyDown={handleKeyDown}
+      />
+      {error && <span className="error-text">{error}</span>}
+    </div>
+  );
+});
